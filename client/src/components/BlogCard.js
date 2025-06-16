607e69bc6a9d542e10f4fab1cd83f6659a7fd505
akhil balla <akhilballa@akhils-MacBook-Air.local>
2025-06-16 11:00:14 +0530

    intial commit

---
+++ client/src/components/BlogCard.js
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import { Box, IconButton, Avatar} from "@mui/material";
+import { Navigate, useNavigate } from "react-router-dom";
+import axios from 'axios';
+import toast from 'react-hot-toast'
+
+export default function BlogCard({title,
+  description,
+  image,
+  username,
+  time,
+  id,
+  isUser
+  }) {
+
+const navigate = useNavigate();
+  const handleEdit = () => {
+    navigate(`/blog-details/${id}`);
+  };
+
+  const handleDelete = async () => {
+    try {
+      const { data } = await axios.delete(`/api/v1/blog/delete-blog/${id}`);
+      if (data?.success) {
+        alert("Blog Deleted");
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Card sx={{
+        width: "40%",
+        margin: "auto",
+        mt: 2,
+        padding: 2,
+        boxShadow: "5px 5px 10px #ccc",
+        ":hover:": {
+          boxShadow: "10px 10px 20px #ccc",
+        },
+      }}>
+
+        {isUser && (
+        <Box display={"flex"}>
+          <IconButton onClick={handleEdit} sx={{ marginLeft: "auto" }}>
+            <ModeEditIcon color="info" />
+          </IconButton>
+          <IconButton onClick={handleDelete}>
+            <DeleteIcon color="error" />
+          </IconButton>
+        </Box>
+      )}
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            {username}
+          </Avatar>
+        }
+        title={username}
+        subheader={time}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={image}
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          {description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+
